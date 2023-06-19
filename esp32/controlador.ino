3120d8a3b5aa7f09b73369220e6a00d11f6b3755
Bernardo Lopes <bernardo_ragonezi@hotmail.com>
2023-06-19 16:20:12 -0300

    prd esp code

--- esp32/controlador.ino
+++ esp32/controlador.ino
@@ -1,9 +1,10 @@
 #include <Arduino.h>
-#include "HX711.h"
 #include <WiFi.h>
 #include <PubSubClient.h>
-#define DT_PIN 4
-#define SCK_PIN 2
+#define option_1 2
+#define option_2 3
+#define option_3 4
+#define option_4 5
 
 
  
@@ -17,38 +18,12 @@ char* payload = ''
 WiFiClient espClient;
 PubSubClient client(espClient);
  
-float offset = 0; //variável para guardar o valor bruto de offset
-float offset_duo = 0;
 
-HX711 scale;
- 
 void setup() {
-  scale.begin(DT_PIN,SCK_PIN);
+
   Serial.begin(9600);
-  offset = scale.read_average(128); //tira uma média de 3 amostras para o offset
-  
-  Serial.println("Before setting up the scale:");
-  Serial.print("read: \t\t");
-  Serial.println(scale.read());			// print a raw reading from the ADC
-
-  Serial.print("read average: \t\t");
-  Serial.println(scale.read_average(20));  	// print the average of 20 readings from the ADC
-
-  Serial.print("get value: \t\t");
-  Serial.println(scale.get_value(5));		// print the average of 5 readings from the ADC minus the tare weight (not set yet)
-
-  Serial.print("get units: \t\t");
-  Serial.println(scale.get_units(5), 1);	// print the average of 5 readings from the ADC minus tare weight (not set) divided
-						// by the SCALE parameter (not set yet)
-
-  scale.set_scale(476.f);                      // this value is obtained by calibrating the scale with known weights; see the README for details
-  scale.tare();				        // reset the scale to 0
-  offset_duo = scale.get_units(10) - offset;
-  offset_duo      /= 100 * -1; 
-
-  Serial.println("COMECANDO MEDICAO");
   WiFi.begin(ssid, password);
- 
+  //Conexao com a rede local
   while (WiFi.waitForConnectResult() != WL_CONNECTED) {
     delay(500);
     Serial.println("Connecting to WiFi..");
@@ -56,7 +31,8 @@ void setup() {
   }
  
   Serial.println("Connected to the WiFi network");
- 
+
+  //Conexao com o servidor MQTT
   client.setServer(mqttServer, mqttPort);
  
   while (!client.connected()) {
@@ -82,11 +58,27 @@ void setup() {
 
 void loop() {
 
-
-
- 
-
   client.loop();
-  delay(2000);
-  client.publish("quizz", String(alternativa).c_str() );
+  if(digitalRead(option_1) == HIGH){
+     Serial.println('1');
+     client.publish("quizz", "[{resposta_tentativa: '1', id_dispositivo: 1}]" );
+     delay(100);
+  }
+  if(digitalRead(option_2) == HIGH){
+     Serial.println('2');
+     client.publish("quizz", "[{resposta_tentativa: '2', id_dispositivo: 1}]" );
+     delay(100);
+  }
+  if(digitalRead(option_3) == HIGH){
+     Serial.println('3');
+     client.publish("quizz", "[{resposta_tentativa: '3', id_dispositivo: 1}]" );
+     delay(100);
+  }
+  if(digitalRead(option_4) == HIGH){
+     Serial.println('4');
+     client.publish("quizz", "[{resposta_tentativa: '4', id_dispositivo: 1}]" );
+     delay(100);
+  }
+  delay(500);
+  
 }
